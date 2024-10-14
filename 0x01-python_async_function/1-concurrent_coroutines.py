a4a0ea43e197ebf266d7868afcfac03f98c8bc7e
charis <charisadu@gmail.com>
2024-10-14 23:45:28 +0100

    update type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ import asyncio
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
 wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without using sort()
