4391b17670313892cc760b6d00e7df51daa69207
charis <charisadu@gmail.com>
2024-10-15 00:42:16 +0100

    type annotate asyncio.task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.tasks:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
 A function (do not create an async function, use the regular function syntax
 to do this) task_wait_random that takes an integer max_delay and returns a
