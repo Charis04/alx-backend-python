0cdf91638d9b79e9ee4538c77fad59f171f25860
charis <charisadu@gmail.com>
2024-10-15 00:19:10 +0100

    remove async from measure_time def

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,8 +8,7 @@ import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
 A function with integers n and max_delay as arguments that measures the total
 execution time for wait_n(n, max_delay), and returns total_time / n. Your
