bbc9c0f1c73e36a16950c9435e9129309d5f6ee5
charis <charisadu@gmail.com>
2024-10-15 00:52:32 +0100

    Take the code from wait_n and alter it into a new function task_wait_n. The code is nearly identical to wait_n except task_wait_random is being called.

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+An async routine called task_wait_n that takes in 2 int arguments (in this order):
+n and max_delay. You will spawn wait_random n times with the specified
+max_delay.
+"""
+
+import asyncio
+from typing import List
+
+task_wait_r = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+task_wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without using sort()
+because of concurrency.
+    """
+    tasks = [task_wait_r(max_delay) for _ in range(n)]
+    delay = await asyncio.gather(*tasks)
+    return sorted(delay)
