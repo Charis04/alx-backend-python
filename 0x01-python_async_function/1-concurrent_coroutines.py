746143376242bf0606984edb4635d82a0182b238
charis <charisadu@gmail.com>
2024-10-14 23:53:17 +0100

    revert approach and update type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,23 +6,16 @@ max_delay.
 """
 
 import asyncio
+from typing import List
 
 wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
 wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without using sort()
  because of concurrency.
     """
-     # Create a list of tasks, each waiting for a random delay
-    tasks = [asyncio.create_task(wait_r(max_delay)) for _ in range(n)]
-
-    # Collect completed results as they finish
-    delays = []
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        delays.append(result)
-
-    return delays
+    delay = await asyncio.gather(*(wait_r(max_delay) for i in range(n)))
+    return delay
